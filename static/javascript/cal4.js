50c8b43014952e13e31831f5262301cbe54f8982
vikasrajyadav <vry420@gmail.com>
2018-04-09 04:36:07 +0530

    commit second debt equity hybrid page update

---
+++ static/javascript/cal4.js
@@ -0,0 +1,162 @@
+var amc_sid_list = [
+  {
+    "amc_code": "IDFCMUTUALFUND_MF",
+    "sid_link": "http://www.idfcmf.com/Downloads.aspx"
+  },
+  {
+    "amc_code": "BNPSUNDARAMPARIBAS_MF",
+    "sid_link": "https://www.sundarambnpparibasfs.in/web/service/home"
+  },
+  {
+    "amc_code": "IDBIMUTUALFUND_MF",
+    "sid_link": "https://www.idbimutual.co.in/Downloads/Downloads"
+  },
+  {
+    "amc_code": "RelianceMutualFund_MF",
+    "sid_link": "https://www.reliancemutual.com/investor-services/downloads/scheme-information-document"
+  },
+  {
+    "amc_code": "DHFLPRAMERICAMUTUALFUND_MF",
+    "sid_link": "https://online.dhflpramericamf.com/"
+  },
+  {
+    "amc_code": "BirlaSunLifeMutualFund_MF",
+    "sid_link": "http://mutualfund.birlasunlife.com/Pages/Individual/Forms-Downloads/Forms.aspx"
+  },
+  {
+    "amc_code": "MOTILALOSWAL_MF",
+    "sid_link": "http://www.motilaloswalmf.com/downloads/mutual-fund/SID"
+  },
+  {
+    "amc_code": "QUANTUMMUTUALFUND_MF",
+    "sid_link": "https://www.quantumamc.com/Downloads.aspx"
+  },
+  {
+    "amc_code": "JM FINANCIAL MUTUAL FUND_MF",
+    "sid_link": "http://www.jmfinancialmf.com/Downloads/Addenda.aspx?SubReportID=538A8B27-62B1-4CFE-BFF2-65DDFF0D4627"
+  },
+  {
+    "amc_code": "DEUTSCHEMUTUALFUND_MF",
+    "sid_link": "https://online.dws-india.com/catalogue/documents/offer/CombinedSIDEquity.pdf"
+  },
+  {
+    "amc_code": "HDFCMutualFund_MF",
+    "sid_link": "http://www.hdfcfund.com/downloads/scheme-information-document"
+  },
+  {
+    "amc_code": "SBIMutualFund_MF",
+    "sid_link": "https://www.sbimf.com/Downloads/OfferDocument.aspx"
+  },
+  {
+    "amc_code": "BNPPARIBAS_MF",
+    "sid_link": "http://www.bnpparibasmf.in/Downloads/index.aspx"
+  },
+  {
+    "amc_code": "KOTAKMAHINDRAMF",
+    "sid_link": "http://assetmanagement.kotak.com/"
+  },
+  {
+    "amc_code": "ICICIPrudentialMutualFund_MF",
+    "sid_link": "http://www.icicipruamc.com/Downloads/sid-mutual-fund.aspx"
+  },
+  {
+    "amc_code": "TATAMutualFund_MF",
+    "sid_link": "http://www.tatamutualfund.com/downloads/scheme-information-document"
+  },
+  {
+    "amc_code": "SHRIRAMMUTUALFUND_MF",
+    "sid_link": "http://www.shriramamc.com/AMCDownloadspdf/SID%20(Final%20corr)-29-10-2013.pdf"
+  },
+  {
+    "amc_code": "L&TMUTUALFUND_MF",
+    "sid_link": "https://www.ltfs.com/companies/lnt-investment-management/downloads.html"
+  },
+  {
+    "amc_code": "MIRAEASSET",
+    "sid_link": "https://www.miraeassetmf.co.in/downloads/forms"
+  },
+  {
+    "amc_code": "GOLDMAN SACHSMUTUALFUND_MF",
+    "sid_link": "http://www.goldmansachs.com/gsam/in/advisors/resources/literature/scheme-information-document/index.html"
+  },
+  {
+    "amc_code": "CANARAROBECOMUTUALFUND_MF",
+    "sid_link": "http://www.canararobeco.com/forms-downloads/pages/SID-Scheme-Information-Document.aspx"
+  },
+  {
+    "amc_code": "TAURUSMUTUALFUND_MF",
+    "sid_link": "https://www.taurusmutualfund.com/Download/FMP.html#"
+  },
+  {
+    "amc_code": "JPMORGANASSETMF",
+    "sid_link": "https://www.jpmorganmf.com/wps/portal/PG_DOWNLOAD_200"
+  },
+  {
+    "amc_code": "RELIGAREMUTUALFUND_MF",
+    "sid_link": "http://www.religareinvesco.com/literature-and-form?tab=Scheme"
+  },
+  {
+    "amc_code": "UTIMUTUALFUND_MF",
+    "sid_link": "http://www.utimf.com/forms-downloads/Pages/default.aspx"
+  },
+  {
+    "amc_code": "IIFLMUTUALFUND_MF",
+    "sid_link": "https://www.iiflmf.com/Downloads/SID.aspx"
+  },
+  {
+    "amc_code": "HSBCMUTUALFUND_MF",
+    "sid_link": "https://www.assetmanagement.hsbc.com/in/attachments/mutual-funds/downloads/sid/cod.pdf"
+  },
+  {
+    "amc_code": "PINEBRIDGEMUTUALFUND_MF",
+    "sid_link": "http://www.pinebridge.com/"
+  },
+  {
+    "amc_code": "SAHARAMUTUALFUND_MF",
+    "sid_link": "http://27.251.188.16/saharamutual/downloads_files/sid/FINAL%20SAHARA%20SID%20SMF%20may%202015.pdf"
+  },
+  {
+    "amc_code": "DSPBLACKROCK",
+    "sid_link": "http://www.dspblackrock.com/investor-centre/download"
+  },
+  {
+    "amc_code": "PEERLESSMUTUALFUND_MF",
+    "sid_link": "http://www.peerlessmf.co.in/dnd_applicationform.html"
+  },
+  {
+    "amc_code": "FRANKLINTEMPLETON",
+    "sid_link": "http://www.franklintempletonindia.com/en_IN/investor/literature/offer-documents"
+  },
+  {
+    "amc_code": "MORGANSTANLEYMUTUALFUND_MF",
+    "sid_link": 0
+  },
+  {
+    "amc_code": "EDELWEISSMUTUALFUND_MF",
+    "sid_link": "http://www.edelweissmf.com/Download/scheme-information-document.aspx"
+  },
+  {
+    "amc_code": "PPFAS_MF",
+    "sid_link": "http://amc.ppfas.com/downloads/scheme-information-document-pltvf.pdf"
+  },
+  {
+    "amc_code": "LICNOMURAMUTUALFUND_MF",
+    "sid_link": "http://www.licnomuramf.com/Investor/Downloads/SID-SAI"
+  },
+  {
+    "amc_code": "AXISMUTUALFUND_MF",
+    "sid_link": "https://www.axismf.com/Downloads.aspx?Value=application"
+  },
+  {
+    "amc_code": "BHARTIAXAMUTUALFUND_MF",
+    "sid_link": "http://www.boiaxa-im.com/downloads/download.php"
+  },
+  {
+    "amc_code": "PRINCIPALMUTUALFUND_MF",
+    "sid_link": "http://www.principalindia.com/downloads/funds.aspx"
+  },
+  {
+    "amc_code": "BARODAPIONEERMUTUALFUND_MF",
+    "sid_link": "http://www.barodapioneer.in/Downloads/pages/sid.aspx"
+  }
+];
